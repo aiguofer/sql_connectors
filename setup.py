6f6a170cb29f74173c9ea2fe7e383e305a37a50d
Diego Fernandez <difernan@redhat.com>
2018-03-20 23:15:55 -0600

    Setup.py improvements

--- setup.py
+++ setup.py
@@ -5,13 +5,35 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
+from codecs import open
+import os
+import re
+
+exec(open('sql_connectors/_version.py').read())
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+vcs = "^(git|hg)\\+"
+
+# Get the long description from the README file
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if not re.search(vcs, x) and x.strip() != '']
+dependency_links = [re.sub(vcs, '', x.strip()) for x in all_reqs if re.search(vcs, x)]
+
+for dep in dependency_links:
+    match = re.search('egg=((?:\w|-)+)', dep)
+    if match:
+        install_requires.append(match.group(1))
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', ]
 
 setup_requirements = [ ]
 
@@ -33,14 +55,9 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     description="A simple wrapper for SQL connections using SQLAlchemy and Pandas read_sql to standardize SQL workflow.",
-    entry_points={
-        'console_scripts': [
-            'sql_connectors=sql_connectors.cli:main',
-        ],
-    },
-    install_requires=requirements,
+    install_requires=install_requires,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=long_description,
     include_package_data=True,
     keywords='sql_connectors',
     name='sql_connectors',
@@ -49,6 +66,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/aiguofer/sql_connectors',
-    version='0.1.0',
+    version=__version__,
     zip_safe=False,
 )
