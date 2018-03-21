d8a0fdd802099e0f006088d069a240c3d5415139
Diego Fernandez <difernan@redhat.com>
2018-03-20 23:30:22 -0600

    Small improvements to __init__.py
    Remove second call to get_available_configs and make configs private

--- sql_connectors/__init__.py
+++ sql_connectors/__init__.py
@@ -1,12 +1,12 @@
 from .config_util import get_available_envs_factory as _envs_factory, get_available_configs as _get_available_configs
 from .client import get_client_factory as _client_factory
 
-configs = _get_available_configs()
+_configs = _get_available_configs()
 
-__all__ = [config for config, default in configs] + \
-          [config + '_envs' for config, default in configs]
+__all__ = [config for config, default in _configs] + \
+          [config + '_envs' for config, default in _configs]
 
-for config_file, defaults in _get_available_configs():
+for config_file, defaults in _configs:
     exec('{0}={1}'.format(config_file,
                           '_client_factory("{0}", "{1}", "{2}")'.format(config_file,
                                                                         defaults['default_env'],
