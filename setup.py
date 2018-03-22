f6da51fa8e6dc9c13bbbafcad5fc05dde1dc9c45
Diego Fernandez <difernan@redhat.com>
2018-03-21 23:59:51 -0600

    Improve git repos in requirements.txt
    By fixing the URL in setup.py, `pip install -r requirements.txt` will
    work and so will regular installs.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ with open(os.path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
 install_requires = [x.strip() for x in all_reqs if not re.search(vcs, x) and x.strip() != '']
-dependency_links = [re.sub(vcs, '', x.strip()) for x in all_reqs if re.search(vcs, x)]
+dependency_links = [re.sub(vcs, '', x.strip()).sub('.git', '/tarball/master') for x in all_reqs if re.search(vcs, x)]
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
